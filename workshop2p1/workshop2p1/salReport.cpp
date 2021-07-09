75c172fdd5fdcf7d273717d6e1f0ecdd0bda71a6
Nasim khodapanah <51764413+nasimkhd@users.noreply.github.com>
2021-07-09 14:51:16 -0400

    Update salReport.cpp

--- workshop2p1/workshop2p1/salReport.cpp
+++ workshop2p1/workshop2p1/salReport.cpp
@@ -1,11 +1,4 @@
-// Workshop 2: 
-// Version: 1.0
-// Date: 2021/5/28
-// Author: Fardad Soleimanloo
-// Description:
-// This file tests the part 1 section of your workshop
-// Do not modify your code
-/////////////////////////////////////////////
+
 #define _CRT_SECURE_NO_WARNINGS
 #include "Employee.h"
 
