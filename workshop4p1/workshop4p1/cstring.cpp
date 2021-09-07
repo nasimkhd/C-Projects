363fc9c5a5844adbd94cc4b8adda58e800c56179
Nasim khodapanah <51764413+nasimkhd@users.noreply.github.com>
2021-09-07 17:45:49 -0400

    Update cstring.cpp

--- workshop4p1/workshop4p1/cstring.cpp
+++ workshop4p1/workshop4p1/cstring.cpp
@@ -1,9 +1,4 @@
-//Name: Nasim Khodapanah
-// //Student ID: 131929200
-// Workshop 4: part 1
-// Email: nkhodapanah@myseneca.ca
-// Date: 2021/6/14
-/////////////////////////////////
+
 #include "cstring.h"
 using namespace std;
 namespace sdds {
