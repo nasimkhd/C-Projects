0ef95a9257f9ff6e8147167140227ace325a8efc
Nasim khodapanah <51764413+nasimkhd@users.noreply.github.com>
2021-09-07 17:45:35 -0400

    Update canisterMain.cpp

--- workshop4p1/workshop4p1/canisterMain.cpp
+++ workshop4p1/workshop4p1/canisterMain.cpp
@@ -1,10 +1,4 @@
-// Workshop 4:
-// Version: 1.0
-// Date: 2021-06-03
-// Author: Fardad Soleimanloo
-// Description:
-// This file tests the lab section of your workshop
-/////////////////////////////////////////////
+
 #include <iostream>
 #include "Canister.h"
 using namespace std;
